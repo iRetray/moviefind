f6610be0c9564cf4c105492df4d4919437920ec9
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-07-13 20:55:12 -0500

    Desarrollo de pagina de inicio

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import { Jumbotron } from 'reactstrap'
+import { Jumbotron, Button } from 'reactstrap'
+import movies from '../images/movies.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilm } from '@fortawesome/free-solid-svg-icons'
 
 export default class Welcome extends React.Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-md-6">
-                    <Jumbotron>
-                        
-                    </Jumbotron>
+            <div>
+                <div className="row justify-content-md-center container-fluid">
+                    <img src={movies} alt="" className="img-fluid" style={{ maxHeight: '500px' }} />
                 </div>
-                <div className="col-md-6">
-
+                <div class="position-sticky">
+                <div className="row justify-content-md-center container-fluid">
+                    <center>
+                    <p className="lead text-light">"El cine es, además de un espectáculo de masas, la imagen en movimiento de la historia de la humanidad"</p>
+                    <Button color="danger" size="lg"><FontAwesomeIcon icon={ faFilm }/> ¡Buscar películas!</Button>
+                    </center>
                 </div>
-
+                </div>
             </div>
         )
     }
