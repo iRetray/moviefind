d88f534be3e430fb9c0b2953ce2daa1575d4df19
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-07-14 13:10:03 -0500

    Diseño de WelcomePage terminada

--- src/components/Carrousell.js
+++ src/components/Carrousell.js
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselIndicators
+} from 'reactstrap';
+import marvel from '../images/marvel.jpg'
+import ready from '../images/ready.jpg'
+import increibles from '../images/increibles.jpg'
+import blood from '../images/blood.jpg'
+import grinch from '../images/grinch.jpg'
 
-export default class Carrousell extends React.Component {
-    render() {
-        return(
-            <div>
-                carrousell
-            </div>
-        )
-    }
+const items = [
+  {
+    src: marvel
+  },
+  {
+    src: ready
+  },
+  {
+    src: increibles
+  },
+  {
+    src: blood
+  },
+  {
+    src: grinch
+  }
+];
+
+const Carrousell = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem className="container-fluid"
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} className="img-fluid"/>
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+    </Carousel>
+  );
 }
+
+export default Carrousell;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -8,35 +8,38 @@ import Carrousell from '../components/Carrousell'
 export default class Welcome extends React.Component {
     render() {
         return (
-            <div className="container-fluid" style={{maxWidth: '90%'}}>
-                <div className="row justify-content-md-center" style={{ marginTop: '30px' }}>
-                    <div className="col-md-8" style={{ padding: '15px' }}>
-                        <Carrousell />
-                    </div>
-                    <div className="col-md-4" style={{ padding: '15px' }}>
-                        <img src={logoBlanco} alt="" className="img-fluid" />
-                        <hr style={{ borderColor: 'white' }}></hr>
-                        <center>
-                            <h3 className="text-white">Una herramienta para buscar y filtrar películas y series.</h3>
-                            <br></br>
-                            <Alert color="secondary">
-                                <h5>Realiza busquedas por actor, género, título, año de producción o calificación, todo gracias a la <strong><i>API REST</i></strong> pública que proporciona <strong><i>TMDB.</i></strong></h5>
-                            </Alert>
-                        </center>
-                        <div className="row">
-                            <div className="col-sm-6" style={{ marginTop: '5px' }}>
-                                <Button color="warning" size="sm" href="" href="https://www.themoviedb.org/documentation/api?language=es" block>
-                                    <FontAwesomeIcon icon={faCode} /> Documentación de <strong>API REST</strong>
-                                </Button>
-                            </div>
-                            <div className="col-sm-6" style={{ marginTop: '5px' }}>
-                                <Button color="warning" size="sm" block>
-                                    <FontAwesomeIcon icon={faNewspaper} /> Página web de <strong>TMDB</strong>
-                                </Button>
-                            </div>
+            <div>
+
+                <div className="container-fluid" style={{ maxWidth: '90%' }}>
+                    <div className="row justify-content-md-center" style={{ marginTop: '30px' }}>
+                        <div className="col-md-8" style={{ padding: '15px' }}>
+                            <Carrousell className="container-fluid" />
                         </div>
-                        <br></br>
-                        <Button color="danger" size="lg" block><FontAwesomeIcon icon={faPlayCircle}></FontAwesomeIcon> <strong>Buscar películas</strong></Button>
+                        <div className="col-md-4" style={{ padding: '15px' }}>
+                            <img src={logoBlanco} alt="" className="img-fluid" />
+                            <hr style={{ borderColor: 'white' }}></hr>
+                            <center>
+                                <h3 className="text-white">Una herramienta para buscar y filtrar películas y series.</h3>
+                                <br></br>
+                                <Alert color="secondary">
+                                    <h5>Realiza busquedas por actor, género, título, año de producción o calificación, todo gracias a la <strong><i>API REST</i></strong> pública que proporciona <strong><i>TMDB.</i></strong></h5>
+                                </Alert>
+                            </center>
+                            <div className="row">
+                                <div className="col-sm-6" style={{ marginTop: '5px' }}>
+                                    <Button color="warning" size="sm" href="https://www.themoviedb.org/documentation/api?language=es" block>
+                                        <FontAwesomeIcon icon={faCode} /> Documentación de <strong>API REST</strong>
+                                    </Button>
+                                </div>
+                                <div className="col-sm-6" style={{ marginTop: '5px' }}>
+                                    <Button color="warning" size="sm" block>
+                                        <FontAwesomeIcon icon={faNewspaper} /> Página web de <strong>TMDB</strong>
+                                    </Button>
+                                </div>
+                            </div>
+                            <br></br>
+                            <Button color="danger" size="lg" block><FontAwesomeIcon icon={faPlayCircle}></FontAwesomeIcon> <strong>Buscar películas</strong></Button>
+                        </div>
                     </div>
                 </div>
             </div>
