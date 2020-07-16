9a4dbc31c7238880c9c7dbf9c2c19c56761979fc
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-07-16 09:33:38 -0500

    Configure responsivity in screenSize

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -18,9 +18,9 @@ export default class SearchResult extends React.Component {
 
     async peticionAPI() {
         const query = this.props.search
-        const pageQuery = "&page="+this.props.page
+        const pageQuery = "&page=" + this.props.page
         const queryBase = "https://api.themoviedb.org/3/search/movie?api_key=32f3a04caefdfe3f72de841ee31a3954&language=es&query="
-        const completeQuery = queryBase.concat(query,pageQuery)
+        const completeQuery = queryBase.concat(query, pageQuery)
         const peticion = await Axios.get(completeQuery)
         this.setState({
             results: peticion.data.results
@@ -29,22 +29,22 @@ export default class SearchResult extends React.Component {
 
     render() {
         return (
-            <div className="row justify-content-md-center">
+            <div className="row">
                 {
                     this.state.results.map(e =>
-                        e.poster_path!==null ?  
-                        <div className="col-md-2" style={{padding: '10px'}} key={e.id}>
-                            <center>
-                            <Movie
-                                title={e.title}
-                                isPosterNull={e.poster_path}
-                                poster={"https://image.tmdb.org/t/p/w500" + e.poster_path}
-                                calificacion={e.vote_average}
-                                id={e.id}
-                            />
-                            </center>
-                        </div>
-                        : <div key={e.id}></div>
+                        e.poster_path !== null ?
+                            <div className="col-sm-6 col-md-4 col-md-4 col-lg-3 col-xl-2 align-self-center" border-color="solid" style={{ borderStyle: 'solid', padding: '10px' }} key={e.id}>
+                                <center>
+                                    <Movie
+                                        title={e.title}
+                                        isPosterNull={e.poster_path}
+                                        poster={"https://image.tmdb.org/t/p/w500" + e.poster_path}
+                                        calificacion={e.vote_average}
+                                        id={e.id}
+                                    />
+                                </center>
+                            </div>
+                            : <div key={e.id}></div>
                     )
                 }
             </div>
